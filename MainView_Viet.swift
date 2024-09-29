eac9d88ee2e8fdfa3b4a312591314968f264d6d9
PrankMyCode123 <pan05102004@gmail.com>
2024-09-29 16:30:53 +0700

    MainView_Viet.swift

--- MainView_Viet.swift
+++ MainView_Viet.swift
@@ -3,34 +3,38 @@
 //  HAHACOFFEE
 //
 //  Created by Đinh Trung Quốc Anh on 21/9/24.
-//
+// Trần Nguyễn Phương An was here on 29/09/2024
 
 import SwiftUI
 
 struct MainView_Viet: View {
-    @Environment(ModelData.self) var modelData
-    
+    @State var selectedTab: String = "home"  // Removed `selectedRecom`
+
     var body: some View {
-        ScrollView(.vertical, showsIndicators: true) {
-            NavigationSplitView {
-                List {
-                    ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                        CategoryRow(categoryName: key, items: modelData.categories[key]!)
-                    }
-                    .listRowInsets(EdgeInsets())
-                }
-                .navigationTitle("Chào Laughters!")
-            } detail: {
-                Text("Select")
+        VStack(spacing: 0) {
+            TabView(selection: $selectedTab) {
+                LandingPage()
+                    .tag("home")
+                Color.pink
+                    .tag("cup")
+                Color.yellow
+                    .tag("bell")
+                Color.red
+                    .tag("cart")
             }
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            .frame(width: 430, height: 900)
+            // Custom Tab Bar
+            CustomTabBar(selectedTab: $selectedTab)
+        }
+        .onAppear {
+            // Move appearance changes to onAppear to avoid thread issues in init
+            UITabBar.appearance().isHidden = true
         }
     }
 }
 
-#Preview {
+struct MainView_Viet_Previews: PreviewProvider {
+    static var previews: some View {
         MainView_Viet()
-        .environment(ModelData())
+            .environmentObject(ModelData())  // Ensure the environment object is available for LandingPage
     }
-
+}
